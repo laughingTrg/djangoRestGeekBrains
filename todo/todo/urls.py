c77763adde30cb09c2669515f131b986e6ad72da
laughing <laughinChrist@gmail.com>
2022-09-21 06:49:16 +0500

    lesson_1 model.Users DefaulRouter

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
+from rest_framework.routers import DefaultRouter
 
+router = DefaultRouter()
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls')),
 ]
