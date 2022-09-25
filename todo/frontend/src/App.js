b69bedbbdf303e2622527da2d2c37f9d7a0260b8
laughing <laughinChrist@gmail.com>
2022-09-25 11:16:12 +0500

    lesson_2 take test data

--- todo/frontend/src/App.js
+++ todo/frontend/src/App.js
@@ -1,25 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import UserList from './components/User.js';
 
 class App extends React.Component {
     constructor(props) {
-        super(props);
+        super(props)
         this.state = {
-            'users' : [],
+            'users': [],
         }
     }
 
-  render() {
-    return (
-        <div>
-            Main app
-        </div>
+    componentDidMount() {
+        const users = [
+            {
+                'username': 'jason',
+                'firstname': 'Артем',
+                'lastname': 'Селиванов',
+                'email': 'selivanov@mail.com',
+            },
+            {
+                'username': 'n1ro',
+                'firstname': 'Сергей',
+                'lastname': 'Кондрашов',
+                'email': 'kondrat@mail.com',
+            },
+            {
+                'username': 'yeahBoy',
+                'firstname': 'Ринат',
+                'lastname': 'Каридов',
+                'email': 'yeahboy@mail.com',
+            },
+        ]
+        this.setState(
+            {
+                'users': users
+            }
+        )
+    }
 
-
-
-  )
-}
+    render() {
+        return (
+            <div>
+                <UserList users={this.state.users} />
+            </div>
+        )
+    }
 }
 
 export default App;
