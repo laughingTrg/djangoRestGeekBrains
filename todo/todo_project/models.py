fc83feaade167b8426112f7e4756888dbce1fa89
laughing <laughinChrist@gmail.com>
2022-09-29 06:51:05 +0500

    create models Project and TODO

--- todo/todo_project/models.py
+++ todo/todo_project/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+from .users.models import Users
 # Create your models here.
+
+# Проект
+class Project (models.Model):
+    name = models.CharField(max_length=128)
+    link = models.URLField
+    users = models.ManyToManyRel(Users)
+
+#Заметка
+class TODO(models.Model):
+    text = models.TextField(max_length=1500)
+    project = models.ManyToOneRel(Project)
+    create_date = models.DateField(auto_now_add=True)
+    update_date = models.DateField()
+    users = models.ForeignKey(Users, models.PROTECT)
+    is_active = models.BooleanField(default=False)
+
+
+
