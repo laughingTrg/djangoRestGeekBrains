4bcabea70f5223e6a9f47f44982f123f54c49f03
laughing <laughinChrist@gmail.com>
2022-10-02 20:59:40 +0500

    lesson_4 ProjectViewSet and pagination and filter_contains

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -140,4 +140,5 @@ AUTH_USER_MODEL = 'todo_users.Users'
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 5,
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
--- todo/todo_project/views.py
+++ todo/todo_project/views.py
@@ -4,10 +4,17 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from .models import Project, TODO
 from .serializers import ProjectModelSerializer, TODOModelSerializer
+from rest_framework.pagination import LimitOffsetPagination
+from .filters import ProjectFilter
+
+class ProjectLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 10
 
 class ProjectModelViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectModelSerializer
+    pagination_class = ProjectLimitOffsetPagination
+    filterset_class = ProjectFilter
 
 
 class TODOModelViewSet(ModelViewSet):
