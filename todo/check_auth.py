8fa7b3647b646a733e9dd1a8d5d47b2794696466
laughing <laughinChrist@gmail.com>
2022-10-13 16:23:07 +0500

    lesson 7 check_auth.py

--- todo/check_auth.py
+++ todo/check_auth.py
@@ -5,9 +5,11 @@ DOMAIN = 'http://localhost:8000'
 def get_url(url):
     return f'{DOMAIN}{url}'
 
+
 response = requests.get(get_url('/api/users'))
 assert response.status_code == 401
 
+#BaseAuth
 response = requests.get(get_url('/api/users'), auth=('admin', 'admin'))
 assert response.status_code == 200
 
@@ -15,12 +17,11 @@ assert response.status_code == 200
 TOKEN = requests.post(get_url('/api-token-auth/'), data={'username': "admin", 'password': "admin"})
 headers = {'Authorization': f'Token {TOKEN.json()["token"]}'}
 response = requests.get(get_url('/api/users'), headers=headers)
-# response = requests.post('api-token-auth/', data={'username': "admin", 'password': "admin"})
 assert response.status_code == 200
 
 
 #Token JWT
-TOKEN = requests.post(get_url('/api/token/'), data={'username': "admin", 'password': "admin"})
+response = requests.post(get_url('/api/token/'), data={'username': "admin", 'password': "admin"})
 result = response.json()
 access = result['access']
 print('First Token', access, end=f'\n{150*"*"}\n')
@@ -30,9 +31,9 @@ print('Second Token', refresh, end=f'\n{150*"*"}\n')
 
 headers = {'Authorization': f'Bearer {access}'}
 response = requests.get(get_url('/api/users'), headers=headers)
-# response = requests.post('api-token-auth/', data={'username': "admin", 'password': "admin"})
+
 assert response.status_code == 200
-# response = requests.get('http://localhost:8000/api/users')
+
 
 print(response.status_code)
 print(response.json())
